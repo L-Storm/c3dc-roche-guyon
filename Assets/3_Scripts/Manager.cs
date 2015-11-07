003848498b8813edc4cf469cbf1c83ffd82301a3
Manon Leterme <manon.leterme@eisti.fr>
2015-11-07 10:55:45 +0100

    Create a dictionary from a layer + Editor

---
+++ Assets/3_Scripts/Manager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class Manager : MonoBehaviour {
+    private static Manager s_current = null;
+    public static Manager Current{
+        get {
+            if (s_current == null) {
+                s_current = GameObject.FindObjectOfType <Manager>();
+                if (s_current == null){
+                        throw new Exception("There is no Manager in the scene");
+                    }
+            }
+            return s_current;
+        }
+    }
+
+    // Instectable objects are on a dictonary with its id and its mode
+    [HideInInspector] public Dictionary<GameObject,Reference> _dictionary = new Dictionary<GameObject, Reference>();
+
+	// Use this for initialization
+	void Start () {
+        // Catch all gameObjects on the layer InspectableObjects
+        GameObject[] _objectsArray = FindGameObjectsWithLayer(LayerMask.NameToLayer("InspectableObjects"));
+        Debug.Log("Nombre d'object inspectable : " + _objectsArray.Length);
+        
+        // Add all these objects on the disctionary
+        for (int i = 0; i< _objectsArray.Length; i++){
+            _dictionary.Add(_objectsArray[i], _objectsArray[i].GetComponent<Parameters>()._ref);
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public GameObject[] FindGameObjectsWithLayer(LayerMask layer)
+    {
+        GameObject[] goArray = FindObjectsOfType(typeof(GameObject)) as GameObject[];
+        var goList = new System.Collections.Generic.List<GameObject>();
+        for (int i = 0; i< goArray.Length; i++)
+        {
+            if (goArray[i].layer == layer)
+            {
+                goList.Add(goArray[i]);
+            }
+        }
+        if (goList.Count == 0)
+            return null;
+        return goList.ToArray();
+    }
+}
+
+public enum Mode { Rotate, Plane };
+
+public class Reference {
+
+    public string _id;
+    public Mode _mode;
+
+    public Reference(string id, Mode mode) {
+        _id = id;
+        _mode = mode;
+    }
+    ~Reference() {}
+}
+
---
+++ Assets/3_Scripts/ManagerEditor.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(Manager))]
+public class ManagerEditor : Editor {
+
+    // For a unique Manager script on the scene
+    public Manager _target = null;
+    protected Manager Target {
+        get {
+            if (_target == null){
+                _target = target as Manager;
+                //_objectsCount = _target._dictionary.Count;
+            }
+            return _target;
+        }
+    }
+
+    private bool _fold = true;
+    //private int _objectsCount;
+    private Vector2 _mousePos;
+
+    public override void OnInspectorGUI(){
+        DrawDefaultInspector();
+
+        GUILayout.BeginVertical();
+        Rect _rectList = GUILayoutUtility.GetRect(new GUIContent("Inspectable Objects"),GUIStyle.none);
+        _fold = EditorGUI.Foldout(_rectList, _fold, "Inspectable Objects");
+        GUILayout.Space(2);
+
+        if (Event.current.type == EventType.DragUpdated){
+            _mousePos = Event.current.mousePosition;
+            DragAndDrop.visualMode = _rectList.Contains(_mousePos) ? DragAndDropVisualMode.Generic : DragAndDropVisualMode.Rejected;
+        }
+        if (Event.current.type == EventType.DragPerform && _rectList.Contains(_mousePos)){
+            if (DragAndDrop.objectReferences.Length != 0){
+                foreach (Object o in DragAndDrop.objectReferences){
+                    if (o.GetType() == typeof(GameObject))
+                        Target._dictionary.Add((GameObject)o, new Reference(null,Mode.Plane));
+                }
+                _fold = true;
+                GUIUtility.ExitGUI();
+                return;            
+            }
+        }
+
+        if (_fold){
+            GUILayout.BeginHorizontal();
+            GUILayout.Space(8);
+            GUILayout.BeginVertical();
+       
+            // Display Inspectable Objects
+            foreach(GameObject obj in Target._dictionary.Keys){
+                GUILayout.BeginHorizontal();
+                GUILayout.Label("Object");
+                GameObject o = (GameObject)EditorGUILayout.ObjectField(obj, typeof(GameObject), true, GUILayout.Width(Screen.width / 4));
+                if (!Target._dictionary.ContainsKey(o)){
+                    Target._dictionary.Add(o, new Reference(null, Mode.Plane));
+                }
+                GUILayout.Label("Id");
+                GUILayout.TextField(obj.GetComponent<Parameters>()._id);
+                //Target._dictionary[obj]._id = EditorGUILayout.TextField(Target._dictionary[obj]._id, GUILayout.Width(Screen.width / 4));
+                GUILayout.Label("Mode");
+                GUILayout.TextField(obj.GetComponent<Parameters>()._mode.ToString());
+                //Mode mod = Target._dictionary[obj]._mode;
+                //mod = (Mode) EditorGUILayout.EnumPopup(mod, GUILayout.Width(Screen.width/4));
+                //if (Target._dictionary[obj]._mode != mod){
+                //    Target._dictionary[obj]._mode = mod;
+                //}
+                GUILayout.EndHorizontal();
+            }
+
+            GUILayout.EndVertical();
+            GUILayout.EndHorizontal();
+        }
+        GUILayout.EndVertical();
+    }
+}

309bdecbde5074ac1f5c17fd7065f1fae726367b
Manon Leterme <manon.leterme@eisti.fr>
2015-11-07 10:57:12 +0100

    Scripts for go'parameters + Editor

---
+++ Assets/3_Scripts/Parameters.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parameters : MonoBehaviour {
+
+    [HideInInspector] public string _id;
+    [HideInInspector] public Mode _mode;
+    [HideInInspector] public Reference _ref = new Reference(null, Mode.Rotate);
+
+	// Use this for initialization
+	void Start () {
+        _ref = new Reference(_id, _mode);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/3_Scripts/ParametersEditor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(Parameters))]
+public class ParametersEditor : Editor {
+    public Parameters _target = null;
+    protected Parameters Target {
+        get
+        {
+            if (_target == null)
+            {
+                _target = target as Parameters;
+            }
+            return _target;
+        }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        GUILayout.BeginVertical();
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Identity : ");
+        Target._id = EditorGUILayout.TextField(Target._id, GUILayout.Width(Screen.width/3));
+        GUILayout.EndHorizontal();
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Mode : ");
+        Mode mod = _target._mode;
+        mod = (Mode)EditorGUILayout.EnumPopup(mod, GUILayout.Width(Screen.width / 3));
+        if (Target._mode != mod)
+        {
+            Target._mode = mod;
+        }
+        GUILayout.EndHorizontal();
+        GUILayout.EndVertical();
+    }
+}
